017bb2f7b8f4a88059e9c212843eb250c944ba98
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-07-01 03:13:44 +0530

    same site none with secure true is neccessary

--- api/config.ts
+++ api/config.ts
@@ -78,7 +78,8 @@ export const transporter = nodemailer.createTransport({
     REFRESH_TOKEN: "refresh_token_from_s",
     OPTIONS: {
       httpOnly: true,
-      secure: isproduction, // Set to true in production if using HTTPS
+      sameSite: 'none',
+      secure: true, // Set to true in production if using HTTPS
       path: '/', // Root path of the domain
       maxAge: 7*24 * 60 * 60 * 1000, // Cookie expiry time in milliseconds (7 day)
        
