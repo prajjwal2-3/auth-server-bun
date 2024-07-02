d18c3829f5a3b3addc3fdd0cf7009042fe2dbf70
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-07-02 23:54:34 +0530

    body

--- api/middlewares/verifyRequest.ts
+++ api/middlewares/verifyRequest.ts
@@ -13,11 +13,11 @@ export const verifyRequest = async (req: any, res: any, next: any) => {
   try {
     
     console.log(req.cookies)
-    
+    console.log(req.body)
    
     
-    const acessToken = req.cookies.access_token_from_s;
-    const refreshToken = req.cookies.refresh_token_from_s;
+    const acessToken = req.body.access_token_from_s;
+    const refreshToken = req.body.refresh_token_from_s;
     console.log(acessToken)
     console.log(refreshToken)
     if (!acessToken || !refreshToken) {
